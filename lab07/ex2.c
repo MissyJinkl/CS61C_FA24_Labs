ae39ad8d96f02c213d6458997b40f09f557cdb11
Missy Jin <missy_jin@berkeley.edu>
2024-11-12 02:03:02 -0800

    update_ex2

--- lab07/ex2.c
+++ lab07/ex2.c
@@ -12,6 +12,14 @@ double dotp_critical(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Use the critical keyword here!
+    #pragma omp parallel for
+    for (int i=0; i < arr_size; i++) {
+        double local_product = x[i] * y[i];
+        #pragma omp critical
+        {
+            global_sum += local_product;
+        }
+    }
     return global_sum;
 }
 
@@ -20,6 +28,10 @@ double dotp_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Use the reduction keyword here!
+    #pragma omp parallel for reduction(+:global_sum)
+    for (int i=0; i < arr_size; i++) {
+        global_sum += x[i] * y[i];
+    }
     return global_sum;
 }
 
@@ -28,5 +40,19 @@ double dotp_manual_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Do NOT use the `reduction` directive here!
+    #pragma omp parallel
+    {
+        double local_sum = 0.0;
+
+        #pragma omp for
+        for (int i = 0; i < arr_size; i++){
+            local_sum += x[i] * y[i];
+        }
+
+        #pragma omp critical
+        {
+            global_sum += local_sum;
+        }
+    }
     return global_sum;
 }
--- lab07/openmp_example.c
+++ lab07/openmp_example.c
@@ -2,6 +2,8 @@
 #include <omp.h>
 
 int main() {
+    const int NUM_THREADS = 3;
+    omp_set_num_threads(NUM_THREADS);
     #pragma omp parallel
     {
         int thread_ID = omp_get_thread_num();
